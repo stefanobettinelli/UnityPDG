8e5a1eec97e61697e75dba27fba64a062aee8d37
stefanobettinelli <stefano.bettinelli85@gmail.com>
2015-04-15 13:27:43 +0200

    some changes commit

--- UnityPDG/Assets/Scripts/DungeonRoom.cs
+++ UnityPDG/Assets/Scripts/DungeonRoom.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class DungeonRoom: MonoBehaviour {
 
+    public DungeonCell dungeonCellPrefab;
+    public WallUnit wallPrefab;
+
     private RoomData data;
     private int minWidth;
     private int maxWidth;
@@ -26,6 +29,26 @@ public class DungeonRoom: MonoBehaviour {
         int aWidth = Random.Range(minWidth, maxWidth);
         int aHeight = Random.Range(minHeight, maxHeight);
         data = new RoomData(aWidth, aHeight);
+    }
+
+    private void InstanciateWall(IntVector2 wallDirection, Direction direction, DungeonCell cell)
+    {
+        WallUnit aWall = Instantiate(wallPrefab) as WallUnit;
+        aWall.transform.parent = cell.transform;
+        aWall.transform.localPosition = new Vector3(wallDirection.x, 0.5f, wallDirection.z);
+        aWall.transform.localRotation = direction.ToRotation();
+
+    }
+
+    private DungeonCell CreateCell(IntVector2 coordinates)
+    {
+        DungeonCell newDungeonCell = Instantiate(dungeonCellPrefab) as DungeonCell;
+        //cells[coordinates.x,coordinates.z] = newDungeonCell;
+        newDungeonCell.name = "Dungeon Cell " + coordinates.x + ", " + coordinates.z;
+        newDungeonCell.Coordinates = coordinates;
+        newDungeonCell.transform.parent = transform; //fa diventare tutte le celle generate figlie del game object Dungeon
+        newDungeonCell.transform.localPosition = new Vector3(coordinates.x + 0.5f, 0f, coordinates.z + 0.5f);
+        return newDungeonCell;
     }
 
     public override string ToString()
